76236e594f41df9b1c73d65d9146233d6dffc92d
ariel-madrid <ariel.argomedo@usach.cl>
2022-06-26 13:42:42 -0400

    Proyecto base

---
+++ mongo/src/test/java/tbd/mongo/MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package tbd.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
